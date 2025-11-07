0b76300f63bcd7ea3c8c2acda59bd2126447fbc4
Nick Forbes-Smith <nick@forbes-smith.com>
2025-11-07 22:13:08 +1100

    mvt working

--- apps/server/src/routes/geometriesRun.ts
+++ apps/server/src/routes/geometriesRun.ts
@@ -5,7 +5,7 @@ import {
   geometryOutputQuerySchema,
   updateGeometriesRunSchema,
 } from '@repo/schemas/crud'
-import { and, count, desc, eq, inArray, SQL } from 'drizzle-orm'
+import { and, desc, eq, inArray, isNotNull, sql, SQL } from 'drizzle-orm'
 import { db } from '~/lib/db'
 import { ServerError } from '~/lib/error'
 import {
@@ -64,8 +64,65 @@ export const fullGeometriesRunSchema = baseGeometriesRunSchema
   .extend({
     productRunCount: z.number().int(),
     outputCount: z.number().int(),
+    bounds: z.object({
+      minX: z.number(),
+      minY: z.number(),
+      maxX: z.number(),
+      maxY: z.number(),
+    }),
   })
   .openapi('GeometriesRunFull')
+
+const TILE_EXTENT = 4096
+const TILE_BUFFER = 64
+
+const fetchGeometryOutputsTile = async ({
+  geometriesRunId,
+  z,
+  x,
+  y,
+}: {
+  geometriesRunId: string
+  z: number
+  x: number
+  y: number
+}) => {
+  const tile = db
+    .select({
+      geometry_output_id: geometryOutput.id,
+      geom: sql<string>`
+        ST_AsMVTGeom(
+          ST_Transform(${geometryOutput.geometry}, 3857),
+          ST_TileEnvelope(${z}, ${x}, ${y}),
+          ${TILE_EXTENT},
+          ${TILE_BUFFER},
+          true
+        )
+      `.as('geom'),
+    })
+    .from(geometryOutput)
+    .where(
+      and(
+        eq(geometryOutput.geometriesRunId, geometriesRunId),
+        sql`ST_Intersects(
+          ST_Transform(${geometryOutput.geometry}, 3857),
+          ST_TileEnvelope(${z}, ${x}, ${y})
+        )`,
+      ),
+    )
+    .as('tile')
+
+  const tileData = await db
+    .select({
+      mvt: sql<Buffer | null>`
+        ST_AsMVT(tile, 'data', ${TILE_EXTENT}, 'geom')
+      `,
+    })
+    .from(tile)
+    .where(isNotNull(tile.geom))
+
+  return tileData[0]?.mvt ?? Buffer.alloc(0)
+}
 
 const geometriesRunNotFoundError = () =>
   new ServerError({
@@ -74,7 +131,7 @@ const geometriesRunNotFoundError = () =>
     description: "geometriesRun you're looking for is not found",
   })
 
-const fetchFullGeometriesRun = async (id: string) => {
+const fetchBaseGeometriesRun = async (id: string) => {
   const geometriesRunRecord = await db.query.geometriesRun.findFirst({
     where: (geometriesRun, { eq }) => eq(geometriesRun.id, id),
     ...fullGeometriesRunQuery,
@@ -84,20 +141,11 @@ const fetchFullGeometriesRun = async (id: string) => {
     return null
   }
 
-  const [outputCount, productRunCount] = await Promise.all([
-    db.$count(geometryOutput, eq(geometryOutput.geometriesRunId, id)),
-    db.$count(productRun, eq(productRun.geometriesRunId, id)),
-  ])
-
-  return {
-    ...geometriesRunRecord,
-    outputCount,
-    productRunCount,
-  }
+  return geometriesRunRecord
 }
 
-const fetchFullGeometriesRunOrThrow = async (id: string) => {
-  const record = await fetchFullGeometriesRun(id)
+const fetchBaseGeometriesRunOrThrow = async (id: string) => {
+  const record = await fetchBaseGeometriesRun(id)
 
   if (!record) {
     throw geometriesRunNotFoundError()
@@ -133,9 +181,36 @@ const app = createOpenAPIApp()
     }),
     async (c) => {
       const { id } = c.req.valid('param')
-      const record = await fetchFullGeometriesRunOrThrow(id)
+      const record = await fetchBaseGeometriesRunOrThrow(id)
 
-      return generateJsonResponse(c, record, 200)
+      const [outputCount, productRunCount] = await Promise.all([
+        db.$count(geometryOutput, eq(geometryOutput.geometriesRunId, id)),
+        db.$count(productRun, eq(productRun.geometriesRunId, id)),
+      ])
+
+      const [bounds] = await db
+        .select({
+          minX: sql<number>`ST_XMin(ST_Extent(${geometryOutput.geometry}))`,
+          minY: sql<number>`ST_YMin(ST_Extent(${geometryOutput.geometry}))`,
+          maxX: sql<number>`ST_XMax(ST_Extent(${geometryOutput.geometry}))`,
+          maxY: sql<number>`ST_YMax(ST_Extent(${geometryOutput.geometry}))`,
+        })
+        .from(geometryOutput)
+        .where(eq(geometryOutput.geometriesRunId, id))
+
+      if (!bounds) {
+        throw new ServerError({
+          statusCode: 500,
+          message: 'Failed to fetch bounds',
+          description: 'Failed to fetch bounds for geometries run',
+        })
+      }
+
+      return generateJsonResponse(
+        c,
+        { ...record, bounds, outputCount, productRunCount },
+        200,
+      )
     },
   )
   .openapi(
@@ -211,6 +286,54 @@ const app = createOpenAPIApp()
         },
         200,
       )
+    },
+  )
+  .openapi(
+    createRoute({
+      description: 'MVT for a geometries run.',
+      method: 'get',
+      path: '/:id/outputs/mvt/:z/:x/:y',
+      middleware: [
+        authMiddleware({
+          permission: 'read:geometryOutput',
+        }),
+      ],
+      request: {
+        params: z.object({
+          id: z.string().min(1),
+          z: z.coerce.number().int(),
+          x: z.coerce.number().int(),
+          y: z.coerce.number().int(),
+        }),
+      },
+      responses: {
+        200: {
+          description: 'Successfully listed outputs for a geometries run.',
+          content: {
+            'image/vnd.mapbox-vector-tile': {
+              schema: z.string(),
+            },
+          },
+        },
+        401: jsonErrorResponse('Unauthorized'),
+        422: validationErrorResponse,
+        500: jsonErrorResponse('Failed to fetch tile'),
+      },
+    }),
+    async (c) => {
+      const { id, z, x, y } = c.req.valid('param')
+
+      const tileBuffer = await fetchGeometryOutputsTile({
+        geometriesRunId: id,
+        z,
+        x,
+        y,
+      })
+
+      return c.body(tileBuffer, 200, {
+        'Content-Type': 'image/vnd.mapbox-vector-tile',
+        'Content-Length': tileBuffer.length.toString(),
+      })
     },
   )
   .openapi(
@@ -299,7 +422,7 @@ const app = createOpenAPIApp()
           description: 'Successfully created a geometries run.',
           content: {
             'application/json': {
-              schema: createResponseSchema(fullGeometriesRunSchema),
+              schema: createResponseSchema(baseGeometriesRunSchema),
             },
           },
         },
@@ -323,7 +446,7 @@ const app = createOpenAPIApp()
         })
       }
 
-      const record = await fetchFullGeometriesRunOrThrow(newGeometriesRun.id)
+      const record = await fetchBaseGeometriesRunOrThrow(newGeometriesRun.id)
 
       return generateJsonResponse(c, record, 201, 'Geometries run created')
     },
@@ -355,7 +478,7 @@ const app = createOpenAPIApp()
           description: 'Successfully updated a geometries run.',
           content: {
             'application/json': {
-              schema: createResponseSchema(fullGeometriesRunSchema),
+              schema: createResponseSchema(baseGeometriesRunSchema),
             },
           },
         },
@@ -379,7 +502,7 @@ const app = createOpenAPIApp()
         throw geometriesRunNotFoundError()
       }
 
-      const fullRecord = await fetchFullGeometriesRunOrThrow(record.id)
+      const fullRecord = await fetchBaseGeometriesRunOrThrow(record.id)
 
       return generateJsonResponse(c, fullRecord, 200, 'Geometries run updated')
     },
@@ -403,7 +526,7 @@ const app = createOpenAPIApp()
           description: 'Successfully deleted a geometries run.',
           content: {
             'application/json': {
-              schema: createResponseSchema(fullGeometriesRunSchema),
+              schema: createResponseSchema(baseGeometriesRunSchema),
             },
           },
         },
@@ -415,7 +538,7 @@ const app = createOpenAPIApp()
     }),
     async (c) => {
       const { id } = c.req.valid('param')
-      const record = await fetchFullGeometriesRunOrThrow(id)
+      const record = await fetchBaseGeometriesRunOrThrow(id)
 
       await db.delete(geometriesRun).where(eq(geometriesRun.id, id))
 
@@ -442,7 +565,7 @@ const app = createOpenAPIApp()
             'Successfully marked a geometries run as the main run for its geometries.',
           content: {
             'application/json': {
-              schema: createResponseSchema(fullGeometriesRunSchema),
+              schema: createResponseSchema(baseGeometriesRunSchema),
             },
           },
         },
@@ -476,7 +599,7 @@ const app = createOpenAPIApp()
         .set({ mainRunId: id })
         .where(eq(geometries.id, run.geometriesId))
 
-      const record = await fetchFullGeometriesRunOrThrow(id)
+      const record = await fetchBaseGeometriesRunOrThrow(id)
 
       return generateJsonResponse(c, record, 200, 'Geometries run set as main')
     },
