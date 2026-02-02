61f2e1f6475fb4a1e2094ac21f15992ae9335ffd
willjnz <will.jones@hotmail.co.nz>
2026-02-02 12:13:21 +1100

    Refactor chain of useEffects to a single useEffect and many useCallbacks

--- apps/web/app/console/dataset/_components/dataset-run-map.tsx
+++ apps/web/app/console/dataset/_components/dataset-run-map.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import { Map } from '@vis.gl/react-maplibre'
 import DeckGL from '@deck.gl/react'
 import { MapViewState, LayersList, Layer } from '@deck.gl/core'
@@ -40,7 +40,6 @@ export const DatasetRunMap = ({
   dataUrl: string | null | undefined
 }) => {
   const [jsTable, setJsTable] = useState<Table | null>(null)
-  const [loading, setLoading] = useState<boolean>(false)
   const [layers, setLayers] = useState<LayersList>([])
   const [selectedFeature, setSelectedFeature] = useState<Feature | null>(null)
   const [viewState, setViewState] = useState<MapViewState>({
@@ -64,139 +63,126 @@ export const DatasetRunMap = ({
   dataUrl = stac_parquet_arrow_s3 // Just for dev. Dataurl should come from props.
   dataType = 'stac-geoparquet' // Just for dev. dataType should come from props.
 
+  // useCallback setters
+  const handleSetJsTable = useCallback((table: Table) => {
+    setJsTable(table)
+  }, [])
+
+  const handleSetLayers = useCallback((layers: LayersList) => {
+    setLayers(layers)
+  }, [])
+
+  const handleSetSelectedFeature = useCallback((feature: Feature | null) => {
+    setSelectedFeature(feature)
+  }, [])
+
+  const handleSetViewState = useCallback((vs: MapViewState) => {
+    setViewState(vs)
+  }, [])
+
+  // Single async useEffect for data loading and layer setup
   useEffect(() => {
-    const fetchParquet = async () => {
-      setLoading(true)
+    let isMounted = true
+    const fetchAndVisualise = async () => {
       try {
         await initParquetWasm()
         const resp = await fetch(dataUrl)
         const arrayBuffer = await resp.arrayBuffer()
         const wasmTable = readParquet(new Uint8Array(arrayBuffer))
         const jsTable = tableFromIPC(wasmTable.intoIPCStream())
-        setJsTable(jsTable)
-      } catch (e) {
-        console.error('Failed to load GeoParquet', e)
-      }
-      setLoading(false)
-    }
-    fetchParquet()
-  }, []) // Don't want dataUrl as hook dependency. Don't want it to run twice, just on initial load.
+        if (!isMounted) return
+        handleSetJsTable(jsTable)
 
-  useEffect(() => {
-    if (!jsTable) return
-    // Detect points or polygons
-    // Geometry column can have different names e.g. "geometry" or "proj:geometry"
-    const geometryVector =
-      jsTable?.getChild('geometry') || jsTable?.getChild('proj:geometry')
-    if (
-      geometryVector?.type?.typeId === 13 // Struct
-      // geometryVector?.type?.children?.length === 2 &&
-      // geometryVector?.type?.children[0]?.name === 'x' &&
-      // geometryVector?.type?.children[1]?.name === 'y'
-    ) {
-      // GeometryType : Point
-      setLayers([
-        new GeoArrowScatterplotLayer({
-          id: 'dataset',
-          data: jsTable,
-          getPosition: geometryVector,
-          getFillColor: fillColor,
-          stroked: true,
-          getLineColor: outlineColor,
-          getLineWidth: (d: Feature) => {
-            const featureId = jsTable?.getChild('id')?.get(d.index)
-            const selectedFeatureId = selectedFeature?.['id']
-            if (featureId == selectedFeatureId) {
-              return 3
-            }
-            return 1
-          },
-          getRadius: (d: Feature) => {
-            const featureId = jsTable?.getChild('id')?.get(d.index)
-            const selectedFeatureId = selectedFeature?.['id']
-            if (featureId == selectedFeatureId) {
-              return 10
-            }
-            return 4
-          },
-          lineWidthUnits: 'pixels',
-          radiusUnits: 'pixels',
-          pickable: true,
-          // TODO: Make onClick a function.
-          onClick: (info) => {
-            if (info && info.object) {
-              setSelectedFeature(
-                Object.fromEntries(Object.entries(info.object)),
-              )
-            }
-          },
-        }),
-      ])
-    } else if (
-      geometryVector?.type?.typeId === 12 // List
-      // geometryVector?.type?.children?.[0]?.type?.typeId === 12 && // List
-      // geometryVector?.type?.children?.[0]?.type?.children?.[0]?.type?.typeId ===
-      //   12 && // List
-      // geometryVector?.type?.children?.[0]?.type?.children?.[0]?.type
-      //   ?.children?.[0]?.type?.typeId === 13 // Struct(x, y)
-    ) {
-      // GeometryType : Polygon
-      setLayers([
-        new GeoArrowPolygonLayer({
-          id: 'dataset',
-          data: jsTable,
-          getPolygon: geometryVector,
-          getFillColor: fillColor,
-          getLineColor: outlineColor,
-          getLineWidth: (d: Feature) => {
-            const featureId = jsTable?.getChild('id')?.get(d.index)
-            const selectedFeatureId = selectedFeature?.['id']
-            if (featureId == selectedFeatureId) {
-              return 5
-            }
-            return 1
-          },
-          lineWidthUnits: 'pixels',
-          lineWidthMinPixels: 1,
-          pickable: true,
-          // TODO: Make onClick a function.
-          onClick: (info) => {
-            if (info && info.object) {
-              setSelectedFeature(
-                Object.fromEntries(Object.entries(info.object)),
-              )
-            }
-          },
-        }),
-      ])
-    } else {
-      console.error('Unknown geometry type in GeoParquet')
-    }
-  }, [jsTable, selectedFeature])
-
-  useEffect(() => {
-    async function loadCOG() {
-      if (dataType == 'stac-geoparquet') {
-        const cogLayerId = 'cog-layer'
-
-        if (!selectedFeature) {
-          // Remove existing COG layer if there is no longer one selected.
-          setLayers((prevLayers: LayersList) =>
-            prevLayers.filter((layer: Layer) => layer.id !== cogLayerId),
-          )
-          return
+        // Detect geometry type
+        const geometryVector =
+          jsTable?.getChild('geometry') || jsTable?.getChild('proj:geometry')
+        let newLayers: LayersList = []
+        if (geometryVector?.type?.typeId === 13) {
+          // Point
+          newLayers = [
+            new GeoArrowScatterplotLayer({
+              id: 'dataset',
+              data: jsTable,
+              getPosition: geometryVector,
+              getFillColor: fillColor,
+              stroked: true,
+              getLineColor: outlineColor,
+              getLineWidth: (d: Feature) => {
+                const featureId = jsTable?.getChild('id')?.get(d.index)
+                const selectedFeatureId = selectedFeature?.['id']
+                if (featureId == selectedFeatureId) {
+                  return 3
+                }
+                return 1
+              },
+              getRadius: (d: Feature) => {
+                const featureId = jsTable?.getChild('id')?.get(d.index)
+                const selectedFeatureId = selectedFeature?.['id']
+                if (featureId == selectedFeatureId) {
+                  return 10
+                }
+                return 4
+              },
+              lineWidthUnits: 'pixels',
+              radiusUnits: 'pixels',
+              pickable: true,
+              onClick: (info) => {
+                if (info && info.object) {
+                  handleSetSelectedFeature(
+                    Object.fromEntries(Object.entries(info.object)),
+                  )
+                }
+              },
+            }),
+          ]
+        } else if (geometryVector?.type?.typeId === 12) {
+          // Polygon
+          newLayers = [
+            new GeoArrowPolygonLayer({
+              id: 'dataset',
+              data: jsTable,
+              getPolygon: geometryVector,
+              getFillColor: fillColor,
+              getLineColor: outlineColor,
+              getLineWidth: (d: Feature) => {
+                const featureId = jsTable?.getChild('id')?.get(d.index)
+                const selectedFeatureId = selectedFeature?.['id']
+                if (featureId == selectedFeatureId) {
+                  return 5
+                }
+                return 1
+              },
+              lineWidthUnits: 'pixels',
+              lineWidthMinPixels: 1,
+              pickable: true,
+              onClick: (info) => {
+                if (info && info.object) {
+                  handleSetSelectedFeature(
+                    Object.fromEntries(Object.entries(info.object)),
+                  )
+                }
+              },
+            }),
+          ]
         } else {
-          // Remove existing COG layer if there is one selected so it can be replaced
-          setLayers((prevLayers: LayersList) =>
-            prevLayers.filter((layer: Layer) => layer.id !== cogLayerId),
+          console.error('Unknown geometry type in GeoParquet')
+        }
+        handleSetLayers(newLayers)
+
+        // COG logic
+        if (dataType === 'stac-geoparquet' && selectedFeature) {
+          const cogLayerId = 'cog-layer'
+          // Remove existing COG layer
+          newLayers = newLayers.filter(
+            (layer: Layer) => layer.id !== cogLayerId,
           )
           // Add new COG layer
-          // This COG is from our S3 bucket. It errors because it is single band instead of 3 band (RGB).
-          // const COG_URL = "https://csdr-public-dev.s3.ap-southeast-2.amazonaws.com/datasets/gmw-v4/data/GMW_N00E008_v4019_mng.tif"
-
           // The visualisation needs 3 bands (rgb), so I made the data fit into 3 bands:
           const COG_URL =
             'https://csdr-public-dev.s3.ap-southeast-2.amazonaws.com/viz-test/GMW_N00E008_v4019_mng_rgb.tif'
+          // This COG is from our S3 bucket. It errors because it is single band instead of 3 band (RGB).
+          // const COG_URL = "https://csdr-public-dev.s3.ap-southeast-2.amazonaws.com/datasets/gmw-v4/data/GMW_N00E008_v4019_mng.tif"
+
           // gdal_translate -of COG -co COMPRESS=DEFLATE -co TILED=YES -co PROFILE=COG \
           //   -b 1 -b 1 -b 1 \
           //   "/Users/wj/Downloads/GMW_N00E008_v4019_mng (3).tif" \
@@ -207,32 +193,34 @@ export const DatasetRunMap = ({
           //   's3://csdr-public-dev',
           //   'https://csdr-public-dev.s3.ap-southeast-2.amazonaws.com',
           // )
-
           const cogLayer = new COGLayer({
             id: cogLayerId,
-            // geotiff: selectedFeatureLink,
             geotiff: COG_URL,
             geoKeysParser,
             onGeoTIFFLoad: (tiff, options) => {
-              console.log('COG loaded', { tiff, options })
-
               const { west, south, east, north } = options.geographicBounds
               const centerLongitude = (west + east) / 2
               const centerLatitude = (south + north) / 2
-              setViewState((vs: MapViewState) => ({
-                ...vs,
+              handleSetViewState({
+                ...viewState,
                 longitude: centerLongitude,
                 latitude: centerLatitude,
                 zoom: 8,
-              }))
+              })
             },
           })
-          setLayers((prevLayers: LayersList) => [...prevLayers, cogLayer])
+          newLayers.push(cogLayer)
+          handleSetLayers(newLayers)
         }
+      } catch (e) {
+        console.error('Failed to load GeoParquet', e)
       }
     }
-    loadCOG()
-  }, [selectedFeature])
+    fetchAndVisualise()
+    return () => {
+      isMounted = false
+    }
+  }, [dataType, dataUrl, selectedFeature])
 
   // {dataType && dataUrl &&
   return (
@@ -250,17 +238,17 @@ export const DatasetRunMap = ({
             mapStyle="https://api.protomaps.com/styles/v5/white/en.json?key=51cf1275231eb004"
           />
         </DeckGL>
-        {loading && (
+        {!jsTable && (
           <div className="absolute top-14 left-2 bg-white p-2 rounded shadow">
             Loading {dataType}...
           </div>
         )}
-        {!loading && dataType === 'stac-geoparquet' && (
+        {jsTable && dataType === 'stac-geoparquet' && (
           <div className="absolute top-2 left-2 bg-white p-2 rounded shadow">
             Click a STAC-Geoparquet Item to view its details and COGs.
           </div>
         )}
-        {!loading && dataType === 'geoparquet' && (
+        {jsTable && dataType === 'geoparquet' && (
           <div className="absolute top-2 left-2 bg-white p-2 rounded shadow">
             Click a Geoparquet Item to view its details.
           </div>
